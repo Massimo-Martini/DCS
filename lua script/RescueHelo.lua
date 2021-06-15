b2b4e5123f2fb7412c25da536d3a9c92b955088d
Massimo M <massimo.m75@gmail.com>
2021-06-15 16:13:20 +0200

    add RescueHelo

---
+++ lua script/RescueHelo.lua
@@ -0,0 +1,18 @@
+-- RescueHelo carrier Truman
+RescueheloTruman=RESCUEHELO:New(UNIT:FindByName("CW-1 H. S. Truman"), "Rescue Helo Truman")
+RescueheloTruman:SetTakeoffAir()
+RescueheloTruman:SetRescueZone(20)
+RescueheloTruman:Start()
+
+-- RescueHelo carrier A. Lincoln
+RescueheloLincoln=RESCUEHELO:New(UNIT:FindByName("CW-9 A. Lincoln"), "Rescue Helo A. Lincoln")
+RescueheloLincoln:SetTakeoffAir()
+RescueheloLincoln:SetRescueZone(20)
+RescueheloLincoln:Start()
+
+-- RescueHelo carrier A. Tarawa
+RescueheloLincoln=RESCUEHELO:New(UNIT:FindByName("LHA-1 Tarawa"), "Rescue Helo Tarawa")
+RescueheloLincoln:SetTakeoffAir()
+RescueheloLincoln:SetRescueZone(20)
+RescueheloLincoln:Start()
+
